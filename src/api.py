438c9c21414ff86c8b5b476490823a11869e7b5a
EvATive7 <evative7@gmail.com>
2025-04-11 13:41:22 +0800

    feat(api): implement request retries and improve caching
    - Add request retries for better error handling
    - Refactor caching logic to be more efficient
    - Create a session object for improved performance

--- src/api.py
+++ src/api.py
@@ -1,55 +1,48 @@
 import logging
-
 import requests
 from diskcache import Cache
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 
 class BestdoriAPI:
     base = "https://bestdori.com/api"
-    logger = logging.getLogger("BestdoriAPI")
-    cache = Cache("cache")
+    _logger = logging.getLogger("BestdoriAPI")
+    _cache = Cache("cache")
+    _session = requests.Session()
+    _adapter = HTTPAdapter(
+        max_retries=Retry(
+            total=3,
+            backoff_factor=2,
+            status_forcelist=[500, 502, 503, 504],
+            connect=5,
+            read=5,
+        )
+    )
+    _session.mount("http://", _adapter)
+    _session.mount("https://", _adapter)
 
     @staticmethod
-    def _set_cache(name, data, expire):
-        BestdoriAPI.cache.set(name, data, expire=expire)
-        BestdoriAPI.logger.info(f"Cache set for {name}")
-        return data
-
-    @staticmethod
-    def _get_cache(name):
-        data = BestdoriAPI.cache.get(name)
-        if data is None:
-            BestdoriAPI.logger.info(f"Cache not found for {name}")
+    def _fetch_and_cache(url, cache_name, expire=None):
+        if cache_ := BestdoriAPI._cache.get(cache_name):
+            BestdoriAPI._logger.info(f"Cache hit for {cache_name}")
+            return cache_
         else:
-            BestdoriAPI.logger.info(f"Cache hit for {name}")
-        return data
+            response = BestdoriAPI._session.get(url).json()
+            BestdoriAPI._cache.set(cache_name, response, expire=expire)
+            BestdoriAPI._logger.info(f"Cache set for {cache_name}")
+            return response
 
     @staticmethod
     def get_song_list():
-        if cache_ := BestdoriAPI._get_cache("allsongs"):
-            return cache_
-        else:
-            response = requests.get(BestdoriAPI.base + "/songs/all.5.json").json()
-            return BestdoriAPI._set_cache(
-                "allsongs",
-                response,
-                expire=3600 * 1,
-            )
+        url = BestdoriAPI.base + "/songs/all.5.json"
+        return BestdoriAPI._fetch_and_cache(url, "allsongs", expire=3600 * 1)
 
     @staticmethod
     def get_chart(song_id: str, difficulty: str):
         cacheid = f"{song_id}-{difficulty}"
-        if cache_ := BestdoriAPI._get_cache(cacheid):
-            return cache_
-        else:
-            response = requests.get(
-                BestdoriAPI.base + f"/charts/{song_id}/{difficulty}.json"
-            ).json()
-            return BestdoriAPI._set_cache(
-                cacheid,
-                response,
-                expire=None,
-            )
+        url = BestdoriAPI.base + f"/charts/{song_id}/{difficulty}.json"
+        return BestdoriAPI._fetch_and_cache(url, cacheid)
 
 
 if __name__ == "__main__":
